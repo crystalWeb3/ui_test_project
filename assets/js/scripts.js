e8132d544a415e284682722f09c421a3a16ca241
Brandon <crystal.apolon@gmail.com>
2024-09-23 17:49:24 -0700

    layout_half_completed

---
+++ assets/js/scripts.js
@@ -0,0 +1,4 @@
+// Load reusable components
+document.getElementById('header-placeholder').innerHTML = '<object type="text/html" data="components/header.html" ></object>';
+document.getElementById('sidebar-placeholder').innerHTML = '<object type="text/html" data="components/sidebar.html" ></object>';
+document.getElementById('footer-placeholder').innerHTML = '<object type="text/html" data="components/footer.html" ></object>';
